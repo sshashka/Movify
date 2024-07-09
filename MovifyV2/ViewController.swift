7d83a4b435cd1583592bc2995dfbaea1ddb6da2c
Oleksandr Vasylenko <sash.vas2001@gmail.com>
2024-07-10 01:48:47 +0300

    Initial Commit

---
+++ MovifyV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovifyV2
+//
+//  Created by Саша Василенко on 10.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
